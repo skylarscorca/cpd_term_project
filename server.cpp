ade9e1ac4bc1c7c6804d8eb30f638deaba27bfcb
jdewey2023 <jcd18c@fsu.edu>
2023-04-26 22:52:47 -0400

    lil fix

--- server.cpp
+++ server.cpp
@@ -102,7 +102,7 @@ void *threadFunc(void *args){
 			} else if(cmd == "dc"){
 				getline(ss, row, ':');
 				getline(ss, col, ':');
-				lines[stoi(row)].erase(stoi(col), 0);
+				lines[stoi(row)].erase(stoi(col), 1);
 			} else{
 				cout << "Error: " << cmd << " is not a valid update type\n";
                 validCMD = false;
