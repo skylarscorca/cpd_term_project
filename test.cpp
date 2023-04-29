6b0701624e78977e98f6c3f9803a64b0fb110feb
Tony Landis Drouillard <drouilla@linprog3.cs.fsu.edu>
2023-04-28 20:56:10 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,29 @@
-// Your First Program
-
+#include <cstring>
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
-int main() {
-    cout << "Hello World!";
-    return 0;
+int main(){
+    stack<char> s;
+    char oneline[100];
+    cout << "Please enter a string (\"quit\" to exit)\n> ";
+    while(cin.getline(oneline, 100)){
+        if(strcmp(oneline, "quit") == 0) break;
+        string forwards = "", backwards = "";
+        for(int i = 0; i < strlen(oneline); ++i){
+            if(isalpha(oneline[i])){
+                forwards += tolower(oneline[i]);
+                s.push(tolower(oneline[i]));
+            }
+        }
+        while(!s.empty()){
+            backwards += s.top();
+            s.pop();
+        }
+        cout << "\n\"" << oneline << "\" ";
+        if(forwards != backwards) cout << " is NOT ";
+        else cout << " IS ";
+        cout << "a palindrome." << endl;
+        cout << "\nPlease enter a string (\"quit\" to exit)\n> ";
+    }
 }
